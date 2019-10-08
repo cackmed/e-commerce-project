4bfeec21fa81b2ba6a64e127eaaa3204873e5c92
Caleb Pendergraft <cackmed@gmail.com>
2019-10-08 10:34:02 -0700

    started styling product page

--- source/products.js
+++ source/products.js
@@ -4,9 +4,8 @@ import renderProducts from './render-products.js';
 const list = document.getElementById('products');
 
 for (let i = 0; i < Products.length; i++) {
-    const products = products[i];
+    let products = Products[i];
     const dom = renderProducts(products);
     list.appendChild(dom);
 
 }
- 
