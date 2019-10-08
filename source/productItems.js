dfb13c4ced1cc58ef12237d5155b030959f30892
Caleb Pendergraft <cackmed@gmail.com>
2019-10-08 11:17:08 -0700

    started customizing products

--- source/productItems.js
+++ source/productItems.js
@@ -1,68 +1,68 @@
 const productExample1 = {
-    id: 'placeholder1',
-    name: 'Example Placeholder',
+    id: 'ironDagger',
+    name: 'Iron Dagger',
     image: 'placeholder1.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'A dagger made of northern iron',
+    category: 'one-handed',
+    price: 10.00,
     cost: 0.25,
 };
 
 const productExample2 = {
-    id: 'placeholder2',
-    name: 'Example Placeholder',
+    id: 'ironBattleAaxe',
+    name: 'Iron Battle Axe',
     image: 'placeholder2.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'A battle axe forged from superior northern iron',
+    category: 'two-handed',
+    price: 55.00,
     cost: 0.25,
 };
 
 const productExample3 = {
-    id: 'placeholder3',
-    name: 'Example Placeholder',
+    id: 'ironGreatSword',
+    name: 'Iron Greatsword',
     image: 'placeholder3.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'A mighty blade forged from superior northern iron',
+    category: 'two-handed',
+    price: 50.00,
     cost: 0.25,
 };
 
 const productExample4 = {
-    id: 'placeholder4',
-    name: 'Example Placeholder',
+    id: 'ironWarhammer',
+    name: 'Iron Warhammer',
     image: 'placeholder4.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'A great hammer forged for war!',
+    category: 'two-handed',
+    price: 60.00,
     cost: 0.25,
 };
 
 const productExample5 = {
-    id: 'placeholder5',
-    name: 'Example Placeholder',
+    id: 'steelDagger',
+    name: 'Steel Dagger',
     image: 'placeholder5.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'one of the most common weapons, forged from steel',
+    category: 'one-handed',
+    price: 18.00,
     cost: 0.25,
 };
 const productExample6 = {
-    id: 'placeholder6',
-    name: 'Example Placeholder',
+    id: 'steelGreatSword',
+    name: 'Steel Greatsword',
     image: 'placeholder6.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'A mighty blade forged from superior northern steel',
+    category: 'two-handed',
+    price: 90.00,
     cost: 0.25,
 };
 const productExample7 = {
-    id: 'placeholder7',
-    name: 'Example Placeholder',
+    id: 'steelSword',
+    name: 'Steel Sword',
     image: 'placeholder7.png',
-    description: 'This is where a description will go',
-    category: 'placeholder',
-    price: 1.00,
+    description: 'a longsword forged from reliable steel',
+    category: 'one-handed',
+    price: 45.00,
     cost: 0.25,
 };
 const productExample8 = {
@@ -74,6 +74,71 @@ const productExample8 = {
     price: 1.00,
     cost: 0.25,
 };
+const productExample9 = {
+    id: 'ironArmor',
+    name: 'Iron Armor',
+    image: 'placeholder9.png',
+    description: 'A chest piece, forged of strong northern iron',
+    category: 'armor',
+    price: 125.00,
+    cost: 0.25,
+};
+const productExample10 = {
+    id: 'ironBoots',
+    name: 'Iron Boots',
+    image: 'placeholder10.png',
+    description: 'Boots forged from iron for the soon to be dead adventuer note: will not protect your knees from arrows',
+    category: 'armor',
+    price: 25.00,
+    cost: 0.25,
+};
+const productExample11 = {
+    id: 'ironGuantlet',
+    name: 'Iron Gauntlets',
+    image: 'placeholder11.png',
+    description: 'Gauntlets forged from iron, for punching the hostile',
+    category: 'armor',
+    price: 25.00,
+    cost: 0.25,
+};
+const productExample12 = {
+    id: 'ironHelmet',
+    name: 'Iron Helmet',
+    image: 'placeholder12.png',
+    description: 'a helmet forged from iron, recommended if survival is a goal',
+    category: 'armor',
+    price: 60.00,
+    cost: 0.25,
+};
+const productExample13 = {
+    id: 'ironShield',
+    name: 'Iron Shield',
+    image: 'placeholder13.png',
+    description: 'a Shield forged from iron, and made of wood, mostly wood',
+    category: 'armor',
+    price: 60.00,
+    cost: 0.25,
+};
+const productExample14 = {
+    id: 'placeholder14',
+    name: 'Example Placeholder',
+    image: 'placeholder14.png',
+    description: 'This is where a description will go',
+    category: 'placeholder',
+    price: 1.00,
+    cost: 0.25,
+};
+const productExample15 = {
+    id: 'placeholder15',
+    name: 'Example Placeholder',
+    image: 'placeholder15.png',
+    description: 'This is where a description will go',
+    category: 'placeholder',
+    price: 1.00,
+    cost: 0.25,
+};
+
+
 const Products = [
     productExample1,
     productExample2,
@@ -83,5 +148,12 @@ const Products = [
     productExample6,
     productExample7,
     productExample8,
+    productExample9,
+    productExample10,
+    productExample11,
+    productExample12,
+    productExample13,
+    productExample14,
+    productExample15,
 ];
 export default Products;
