c07c4388f649fd943051df635af306bfd87449c3
Caleb Pendergraft <cackmed@gmail.com>
2019-10-10 11:59:47 -0700

    still working on getting local storage cart to display on shopping cart page

--- shoping-cart/shopping-cart.js
+++ shoping-cart/shopping-cart.js
@@ -1,10 +1,10 @@
-import cart from '../data/carts.js';
 import Products from '../data/productItems.js';
 import { findById, calcOrderTotal, toUSD } from '../common/utils.js';
 import renderLineItem from './render-line-item.js';
 import { localCartData } from '../products/render-products.js';
 
 const tbody = document.querySelector('tbody');
+
 export const getById = (id, Products) => {
     let matchingProducts;
     Products.forEach(Products => {
@@ -15,15 +15,27 @@ export const getById = (id, Products) => {
     return matchingProducts; 
 };
 const orderTotalCell = document.getElementById('order-total-cell');
-
-for (let i = 0; i < cart.length; i++) {
-    const lineItem = cart[i];
-    const weapon = findById(Products, lineItem.id);
-    const dom = renderLineItem(lineItem, weapon);
-
-    tbody.appendChild(dom);
-}
 orderTotalCell.textContent = toUSD(calcOrderTotal(cart, Products));
 
-const javascriptReadableCart = JSON.parse(localStorage.getItem(localCartData));
-buildTable(javascriptReadableCart, Products);
+const addRow = (productsOrder, Products) => {
+    const orderProduct = getById(productsOrder.id, Products);
+    const row = renderLineItem(orderProduct, productsOrder);
+
+    tbody.appendChild(row);
+};
+const addRows = (cart, Products) => {
+    cart.forEach(productsOrder => {
+        addRow(productsOrder, Products);
+    });
+};
+
+const javascriptCart = JSON.parse(localStorage.getItem(localCartData));
+buildTable(javascriptCart, Products);
+
+//for (let i = 0; i < cart.length; i++) {
+//    const lineItem = cart[i];
+//    const weapon = findById(Products, lineItem.id);
+//    const dom = renderLineItem(lineItem, weapon);
+//
+//    tbody.appendChild(dom);
+//}
